d539a21babbd4f2f86818ae35844949bbe1c626b
elkaix <melkholy@techmatrix.com>
2026-08-16 20:10:13 -0400

    chore(site): remove the marketing site

--- apps/site/src/install-channels.js
+++
@@ -1,7 +0,0 @@
-export const INSTALL_CHANNELS = [
-  { id: 'unix', label: 'Mac / Linux', command: 'curl -fsSL https://code.pythinker.com/pythinker-code/install.sh | bash', icon: '/brand/apple.svg' },
-  { id: 'windows', label: 'Windows', command: 'irm https://code.pythinker.com/pythinker-code/install.ps1 | iex', icon: '/brand/windows11.svg' },
-  { id: 'brew', label: 'Homebrew', command: 'brew install pymodel/tap/pythinker-code' },
-  { id: 'nix', label: 'Nix', command: 'nix run github:PyModel/pythinker-code' },
-  { id: 'npm', label: 'npm', command: 'npm install -g @pymodel/pythinker-code', icon: '/brand/npm.svg' },
-];
