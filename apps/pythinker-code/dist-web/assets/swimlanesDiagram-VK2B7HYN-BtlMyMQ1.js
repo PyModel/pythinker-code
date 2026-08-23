7f0d5ad3d87a5740a60e9946b08ccc8c3261bb30
elkaix <melkholy@techmatrix.com>
2026-08-23 00:57:16 -0400

    build(web): stage Linux desktop assets
    Use the Linux-generated bundle that the artifact security job reproduces during desktop packaging.
    
    Task: tasks/todo.md desktop updater consent and UX

---
+++ apps/pythinker-code/dist-web/assets/swimlanesDiagram-VK2B7HYN-BtlMyMQ1.js
@@ -0,0 +1,8 @@
+import{c as e,s as r}from"./flowDiagram-A5DVABFB-93_AFKE1.js";import{_ as a}from"./mermaidParser.worker-DimdFOud.js";import"./chunk-5VM5RSS4-B6TwRhtM.js";import"./chunk-XXDRQBXY-O3ugxnCs.js";import"./chunk-POPQ4Y6H-ClOHKyvb.js";import"./chunk-F27PBJKO-CYKbU5Oh.js";import"./channel-BKkxHUFp.js";var o=a(t=>`${r(t)}
+  .swimlane.cluster rect {
+    stroke: ${t.clusterBorder} !important;
+  }
+  [data-look="neo"].cluster rect {
+    filter: none;
+  }
+`,"getStyles"),s=o,f=e({defaultLayout:"swimlane",styles:s});export{f as diagram};
