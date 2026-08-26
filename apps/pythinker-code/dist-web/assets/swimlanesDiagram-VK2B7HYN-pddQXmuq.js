2d76cad8ce6925320eb1f074e8690baca9618107
M Elkholy <melkholy@techmatrix.com>
2026-08-26 06:15:34 -0400

    fix(web): correct two-prompt anchor selection (#204)

---
+++ apps/pythinker-code/dist-web/assets/swimlanesDiagram-VK2B7HYN-pddQXmuq.js
@@ -0,0 +1,8 @@
+import{c as r,s as e}from"./flowDiagram-A5DVABFB-B55b-50s.js";import{_ as a}from"./mermaid.core-D6Xg32pF.js";import"./chunk-5VM5RSS4-baBluNR7.js";import"./chunk-XXDRQBXY-BOyQwG-7.js";import"./chunk-POPQ4Y6H-Tp7S0w--.js";import"./chunk-F27PBJKO-B_4WdsPc.js";import"./purify.es-5AjVNlXF.js";import"./channel-CRmZXxAS.js";import"./index-D9Nz1t7z.js";var o=a(t=>`${e(t)}
+  .swimlane.cluster rect {
+    stroke: ${t.clusterBorder} !important;
+  }
+  [data-look="neo"].cluster rect {
+    filter: none;
+  }
+`,"getStyles"),m=o,y=r({defaultLayout:"swimlane",styles:m});export{y as diagram};
