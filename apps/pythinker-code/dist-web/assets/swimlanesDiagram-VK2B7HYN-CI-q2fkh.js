5799e7b19d73b4d669093b55226f0c847fbaea7f
elkaix <melkholy@techmatrix.com>
2026-08-28 13:19:33 -0400

    fix(web): let patchSubagent skip undefined fields instead of conditional spreads

---
+++ apps/pythinker-code/dist-web/assets/swimlanesDiagram-VK2B7HYN-CI-q2fkh.js
@@ -0,0 +1,8 @@
+import{c as r,s as e}from"./flowDiagram-A5DVABFB-g14n2cnp.js";import{_ as a}from"./mermaid.core-DLjgMcEc.js";import"./chunk-5VM5RSS4-yrwbFV_l.js";import"./chunk-XXDRQBXY-Q42PP6uX.js";import"./chunk-POPQ4Y6H-DsgrhPSl.js";import"./chunk-F27PBJKO-C-bf8Su7.js";import"./purify.es-5AjVNlXF.js";import"./channel-DeyMK50X.js";import"./index-B9x9_xrb.js";var o=a(t=>`${e(t)}
+  .swimlane.cluster rect {
+    stroke: ${t.clusterBorder} !important;
+  }
+  [data-look="neo"].cluster rect {
+    filter: none;
+  }
+`,"getStyles"),m=o,y=r({defaultLayout:"swimlane",styles:m});export{y as diagram};
--- apps/pythinker-code/dist-web/assets/swimlanesDiagram-VK2B7HYN-Cj3oi6qD.js
+++
@@ -1,8 +0,0 @@
-import{c as r,s as e}from"./flowDiagram-A5DVABFB-Bl5HC-un.js";import{_ as a}from"./mermaid.core-p-4vZBpL.js";import"./chunk-5VM5RSS4-TaulpBEh.js";import"./chunk-XXDRQBXY-DN1oMDA-.js";import"./chunk-POPQ4Y6H-TSaFr0CL.js";import"./chunk-F27PBJKO-DxTDaWxV.js";import"./purify.es-5AjVNlXF.js";import"./channel-DRACu3XI.js";import"./index-LbmhAqtd.js";var o=a(t=>`${e(t)}
-  .swimlane.cluster rect {
-    stroke: ${t.clusterBorder} !important;
-  }
-  [data-look="neo"].cluster rect {
-    filter: none;
-  }
-`,"getStyles"),m=o,y=r({defaultLayout:"swimlane",styles:m});export{y as diagram};
