4bc15895494b0625a44c088ffc963c32cc0f01ab
elkaix <melkholy@techmatrix.com>
2026-09-02 13:38:52 -0400

    build: update Nix dependency hash

--- flake.nix
+++ flake.nix
@@ -160,7 +160,7 @@
               inherit pnpm;
               fetcherVersion = 3;
               # Monaco's package patch is part of src, not the fetched dependency closure.
-              hash = "sha256-tk24c3xE4B41XI3Wy2sPdTALQjlP/4I/JDx1j8Q9+/8=";
+              hash = "sha256-U0VI6DXaPoCmrCBxP8IvluYYcoXoEZ9dUu/cWYebJ5Y=";
             };
 
             nativeBuildInputs = [
